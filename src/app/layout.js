6e7cccf08555b0ce477fb3043a772ea8847c1e2b
matiaslapolla <matias.lapolla.1@gmail.com>
2023-05-12 00:56:32 -0300

    renamed project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,14 @@
-import "./globals.css"
+import "./globals.css";
 
 export const metadata = {
-	title: "Sidequix",
-	description: "Find the best side hustles for you",
+	title: "Tution",
+	description: "El lugar de Tu Tienda Online",
 };
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="es">
 			<body>{children}</body>
 		</html>
 	);
 }
- 
